9d6ec48b6cf81aaab464189bbe82c43204fdec5c
Arivoli.A.E. <aehari2010@gmail.com>
2026-08-01 15:03:44 -0700

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    // saving some efforts using maps
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> seen = new HashMap<>();
+
+        if (s.length() <= 1) return s.length();
+
+        int left = 0, right = 0;
+        int maxLength = 1;
+
+        while (left < s.length() && right < s.length()) {
+            if (seen.containsKey(s.charAt(right))) {                
+                int substringStart = seen.get(s.charAt(right)) + 1;
+                left = left < substringStart ? substringStart : left;
+                
+            } 
+
+            seen.put(s.charAt(right), right);
+            maxLength = Math.max(right-left + 1, maxLength);      
+            right++;          
+
+           // System.out.println(maxLength + " : " + seen);
+        }
+
+        return maxLength;
+    }
+
+
+    // public int lengthOfLongestSubstring(String s) {
+
+    //     HashSet<Character> seen = new HashSet<>();
+
+    //     if (s.length() <= 1) return s.length();
+
+    //     int left = 0, right = 0;
+    //     int maxLength = 1;
+
+    //     while (left < s.length() && right < s.length()) {
+    //         if (!seen.contains(s.charAt(right))) {
+    //             seen.add(s.charAt(right));
+    //             maxLength = Math.max(right-left + 1, maxLength);      
+    //             right++;          
+    //         } else {
+    //             seen.remove(s.charAt(left));
+    //             left++;
+    //         }
+
+    //       //  System.out.println(right + " : " + seen);
+    //     }
+
+    //     return maxLength;
+        
+    // }
+}
