86ebc7dafc2f593121dd5697088bcaf8d8991606
Arivoli.A.E. <aehari2010@gmail.com>
2026-08-01 14:03:34 -0700

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int pivotIndex = findPivot(nums);
+        System.out.println(pivotIndex);
+        int leftResult = binarySearch(nums, 0, pivotIndex, target);
+        int rightResult = binarySearch(nums, pivotIndex+1, nums.length-1, target);
+
+        return leftResult != -1 ? leftResult : rightResult;
+    }
+
+    public int binarySearch(int[] nums, int left, int right, int target) {
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public int findPivot(int[] nums) {
+        int left = 0, right = nums.length-1;
+
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            //System.out.println(nums[left] + ", " + nums[mid] + ", " + nums[right]);
+
+
+            if (mid != nums.length-1 && nums[mid] > nums[mid+1])   {
+                return mid;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        
+        return left;
+    }
+}
