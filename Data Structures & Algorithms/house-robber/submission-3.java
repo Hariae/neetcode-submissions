b35c1d9d7a35ecaf4300c705a908be5ae9a2f8f7
Arivoli.A.E. <aehari2010@gmail.com>
2026-08-18 07:14:28 -0700

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {  
+        int[] cache = new int[nums.length + 2];
+        Arrays.fill(cache, -1);
+        
+        return robUtil(nums, 0, cache);
+    }
+
+    public int robUtil(int[] nums, int index, int[] cache) {
+        
+        if (index >= nums.length) return 0;
+
+        if (cache[index] != -1) {
+            return cache[index];
+        }
+
+
+        cache[index] = Math.max(
+            robUtil(nums, index + 1, cache), 
+            robUtil(nums, index + 2, cache) + nums[index]);
+        
+        return cache[index];
+       
+
+    }
+}
