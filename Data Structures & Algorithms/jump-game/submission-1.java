509cd96ad0a79caa1d5e9b606ddd6695c9942b5e
Arivoli.A.E. <aehari2010@gmail.com>
2026-08-22 12:36:27 -0700

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,50 @@
+class Solution {
+
+
+    /*
+    
+    [1, 2, 0, 1, 0]
+        * 
+    
+    */
+    // public boolean canJump(int[] nums) {
+    //     boolean[] greedy = new boolean[nums.length];
+    //     greedy[nums.length-1] = true;
+    //     for (int i=nums.length-2;i>=0;i--) {
+    //         int steps = nums[i];
+    //         int curr = i;
+            
+    //         while (steps > 0) {
+    //             if ((curr + steps) >= nums.length-1 || greedy[curr + steps] == true) {
+    //                 greedy[curr] = true;
+    //                 //System.out.println(steps + " " + curr);
+    //                 break;
+    //             } else {
+    //                 steps--;
+    //             }
+    //         }   
+    //         //System.out.println(Arrays.toString(greedy));        
+    //     }
+    //     //System.out.println(Arrays.toString(greedy));
+    //     return greedy[0]; 
+    // }
+
+    public boolean canJump(int[] nums) {
+        
+        int lastCanJump = nums.length - 1;
+        
+        for (int i=nums.length-2;i>=0;i--) {
+            int steps = nums[i];
+            int curr = i;
+            
+
+            if (curr+steps >= lastCanJump) {
+                lastCanJump = i;
+            }
+
+                          
+        }
+        //System.out.println(Arrays.toString(greedy));
+        return lastCanJump == 0; 
+    }
+}
