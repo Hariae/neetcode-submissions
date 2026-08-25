c498cfe0a0b541e5ba79adefb804e9f6b3a1c706
Arivoli.A.E. <aehari2010@gmail.com>
2026-08-25 06:31:57 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll(" ", "");
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        System.out.println(s);
+
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+        
+    }
+}
